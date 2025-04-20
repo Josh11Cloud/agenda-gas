fa93059725a73d821a6ce81903f516051206721e
Josh11Cloud <pew3763@gmail.com>
2025-04-19 18:33:59 -0600

    video 9

--- funciones.js
+++ funciones.js
@@ -5,3 +5,9 @@ function doGet() {
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1n6vcn_NK_sK2rF05NeZz1Yt9EKfDzw1uHUY_9JZ59a0').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
